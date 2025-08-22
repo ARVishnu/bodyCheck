63f8ed4ddc7b71e09b830be7f3afacc02610dd29
Vishnu Arora <vishnu.enest@gmail.com>
2025-08-22 13:29:41 +0000

    Admin chamges minor

--- AdminUI/src/components/AdminLayout.tsx
+++ AdminUI/src/components/AdminLayout.tsx
@@ -5,7 +5,7 @@ import { darkLogo } from '../assets'
 export function AdminLayout(): JSX.Element {
   return (
     <div className='min-h-screen bg-gray-50 flex flex-col'>
-      <NavLink to="/" className='px-4 sm:px-6 lg:px-8 py-4 bg-white shadow sticky top-0 z-50'>
+      <NavLink to="/" className='px-4 sm:px-6 lg:px-8 bg-white shadow sticky top-0 z-50'>
       <img src={darkLogo} alt="" style={{width:"250px"}} />
       </NavLink>
       <div className="px-4 sm:px-6 lg:px-8">
